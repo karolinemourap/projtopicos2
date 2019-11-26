5ca17cee08aa8114948f498bbb2db8ce945fd9d4
karolinemourap <58141706+karolinemourap@users.noreply.github.com>
2019-11-26 20:34:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="CSS/uploadfilemulti.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<div class="container">
+  <div class="jumbotron">
+  <h2>Upload</h2>
+    <button type="button" id="mulitplefileuploader">Importar arquivo(s)</button>
+  </div>
+  </br>
+</br>
+ 
+    <button type="button"><a href="arquivos.php">Listar arquivos</a></button>
+  </div>
+</div>
+
+</div>
+</body>
+</html>
+</html>
+<script src="JS/jquery.fileuploadmulti.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function()
+     {
+
+     var settings = {
+        url: "importar.php",
+        method: "POST",
+        allowedTypes:"pdf",
+        fileName: "file",
+        multiple: true,
+        
+        onSuccess:function(files,data,xhr)
+        {
+           //faz alguma coisa
+
+        },
+     
+         afterUploadAll:function()
+         {
+            $(".upload-bar").css("animation-play-state","paused");
+            
+         },
+        onError: function(files,status,errMsg)
+        {       
+          
+            alert(errMsg);
+        }
+
+        
+     }
+     $("#mulitplefileuploader").uploadFile(settings);
+        
+     });
+</script>
